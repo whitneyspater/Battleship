f60170521b59fad6b3cd580dbd07c964e94ffe19
whitneyspater <spatwb21@wfu.edu>
2021-11-29 14:49:27 -0500

    new

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,5 +1,49 @@
+
 public class Battleship {
     public static void main(String[] args) {
+        int player = 1;
+        String[][] player1 = new String [10][10];
+        String[][] player2 = new String [10][10];
+
+        int[][] p1Guessed = new int [10][10];
+        int[][] p2Guessed = new int [10][10];
+
+        Ship shipA = new Ship(5);
+        Ship shipB = new Ship(4);
+        Ship shipC = new Ship(3);
+        Ship shipD = new Ship(3);
+        Ship shipE = new Ship();
+
+        drawBoard();
+        placeShips();
+        guessShips();
+        drawShips(player);
+
+        //switches player
+        if(player == 1){
+            player = 2;
+        }
+        else{
+            player = 1;
+        }
 
     }
+
+    //draws the 10 by 10 board onto the screen
+    public static void drawBoard(){
+        //DRAW METHOD HERE
+    }
+
+    public static void placeShips(){
+        //METHOD TO ALLOW USERS TO PUT THEIR SHIPS IN THE SPOTS THEY CHOOSE
+    }
+
+    public static void guessShips(){
+        //USER GUESSES WHERE OTHER PLAYER PLACED THEIR SHIPS
+    }
+
+    public static void drawShips(int player){
+
+    }
+
 }
---
+++ src/Ship.java
@@ -0,0 +1,60 @@
+public class Ship {
+    private int xLoc1;
+    private int yLoc1;
+    private int xLoc2;
+    private int yLoc2;
+    private int length;
+
+    public Ship(){
+        this.xLoc1 = -1;
+        this.yLoc1 = -1;
+        this.xLoc2 = -3;
+        this.yLoc2 = -3;
+        this.length = 2;
+    }
+
+    public Ship(int length){
+        this.xLoc1 = -1;
+        this.yLoc1 = -1;
+        this.xLoc2 = -1 - length;
+        this.yLoc2 = -1 - length;
+        this.length = length;
+    }
+
+    public int getX1(){
+        return xLoc1;
+    }
+
+    public int getY1(){
+        return yLoc1;
+    }
+
+    public int getX2(){
+        return xLoc2;
+    }
+
+    public int getY2(){
+        return yLoc2;
+    }
+
+    public int getLength(){
+        return length;
+    }
+
+    public void setX1(int x1){
+        this.xLoc1 = x1;
+    }
+
+    public void setY1(int y1){
+        this.yLoc1 = y1;
+    }
+
+    public void setX2(int x2){
+        this.xLoc2 = x2;
+    }
+
+    public void setY2(int y2){
+        this.yLoc2 = y2;
+    }
+
+}
